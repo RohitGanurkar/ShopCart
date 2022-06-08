625afeebf59c352a5f9f6dab8c16e6fff7698f7b
Rahul <rketu786@gmail.com>
2022-06-08 23:34:09 +0530

    implement -> ProductDetailActivity

--- app/src/main/java/com/example/shopcart/activitys/ProductDetailActivity.java
+++ app/src/main/java/com/example/shopcart/activitys/ProductDetailActivity.java
@@ -3,14 +3,79 @@ package com.example.shopcart.activitys;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.view.Menu;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.example.shopcart.R;
+import com.example.shopcart.databinding.ActivityProductDetailBinding;
+import com.example.shopcart.utils.Constants;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ProductDetailActivity extends AppCompatActivity {
-
+    ActivityProductDetailBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_detail);
+        binding = ActivityProductDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        String name = getIntent().getStringExtra("name");
+        String image = getIntent().getStringExtra("image");
+        int id = getIntent().getIntExtra("id",0);
+        double price = getIntent().getDoubleExtra("price",0);
+
+        Glide.with(this)
+                .load(image)
+                .placeholder(R.drawable.placeholder)
+                .into(binding.productImage);
+        binding.productDescription.setText("PRODUCT");
+
+        getProductDetails(id);
+
+        getSupportActionBar().setTitle(name);
+
+        // for Default Back Button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    void getProductDetails(int id){
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = Constants.GET_PRODUCT_DETAILS_URL + id ;
+        StringRequest request = new StringRequest(Request.Method.GET, url, response -> {
+            try {
+                JSONObject object = new JSONObject(response);
+                if(object.getString("status").equals("success")){
+                    JSONObject product = object.getJSONObject("product");
+                    String description = product.getString("description");
+                    binding.productDescription.setText(Html.fromHtml(description));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> {
+
+        });
+        queue.add(request);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.cart, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
